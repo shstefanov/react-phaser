e60d6b73811fa6eb3bb5f7d63406204949cf2133
Stefan Stefanov <s.h.stefanov@mail.bg>
2024-10-05 04:34:17 +0000

    Adding index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export { default as PhaserGame   } from "./src/PhaserGame.component.jsx";
+export { default as PhaserScene  } from "./src/PhaserScene.component.jsx";
+export { default as PhaserObject } from "./src/PhaserObject.component.jsx";
+
+export { useGame   } from "./src/PhaserGame.component.jsx";
+export { useScene  } from "./src/PhaserScene.component.jsx";
+export { useObject } from "./src/PhaserObject.component.jsx";
