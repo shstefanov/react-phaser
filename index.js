09a7cd4f4143cb69b3ccc0b755423acb85ed77cd
Stefan Stefanov <s.h.stefanov@mail.bg>
2024-10-05 04:34:42 +0000

    Using full filenames in main file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-export { default as PhaserGame   } from "./src/PhaserGame.component";
-export { default as PhaserScene  } from "./src/PhaserScene.component";
-export { default as PhaserObject } from "./src/PhaserObject.component";
+export { default as PhaserGame   } from "./src/PhaserGame.component.jsx";
+export { default as PhaserScene  } from "./src/PhaserScene.component.jsx";
+export { default as PhaserObject } from "./src/PhaserObject.component.jsx";
 
-export { useGame   } from "./src/PhaserGame.component";
-export { useScene  } from "./src/PhaserScene.component";
-export { useObject } from "./src/PhaserObject.component";
+export { useGame   } from "./src/PhaserGame.component.jsx";
+export { useScene  } from "./src/PhaserScene.component.jsx";
+export { useObject } from "./src/PhaserObject.component.jsx";
